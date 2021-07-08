d4f66412e8bf3381a2a38a11ffd739a21e69b8ba
francescdv <francescdv@gmail.com>
2021-07-08 12:47:54 +0200

    no message

--- controller.js
+++ controller.js
@@ -1,10 +1,14 @@
+var urlChuck = "http://api.icndb.com/jokes/random";
 
-$(document).ready(function () {
+
+//Nivell 1 i 2
+
+/*$(document).ready(function () {
     $('#jokes').click(function (e) { 
         e.preventDefault();
        $.ajax({
            type:'post',
-           url:'http://api.icndb.com/jokes/random',
+           url:urlChuck,
            dataType:'json',
            success: function (data) {
                $('#joke-text').html(data.value.joke);
@@ -19,4 +23,22 @@ $(document).ready(function () {
        
         
     });
+});
+
+*/
+
+//Nivell 3
+
+$(document).ready(function () {
+  $('#jokes').click(function (e) {
+    e.preventDefault();
+
+    fetch(urlChuck)
+      .then((response) => response.json())
+      .then((data) => {
+        $('#joke-text').html(data.value.joke);
+      })
+
+      .catch((err) => console.log(err));
+  });
 });
